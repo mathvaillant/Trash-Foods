e18d78bb133f5eab61c4da202175b989f4f785bf
mathvaillant <matheusvaillant505@gmail.com>
2024-03-09 08:50:30 +0000

    Initial Commit

---
+++ Trash Food/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trash Food
+//
+//  Created by Matheus Vaillant on 09/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trash Food/Trash_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trash_FoodApp.swift
+//  Trash Food
+//
+//  Created by Matheus Vaillant on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Trash_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
